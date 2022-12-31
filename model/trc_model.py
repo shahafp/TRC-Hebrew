9b08b3b5818c250ee7a7faa51a49e4421c6fa4d6
shahafpariente <shahaf.pariente@stargo.co>
2022-12-31 13:30:01 +0200

    trc model finish class structure

--- model/trc_model.py
+++ model/trc_model.py
@@ -15,8 +15,8 @@ class TRCModel(nn.Module):
 
     def forward(self, text,
                 entity_1, entity_2,
-                entity_mark_1_s, entity_mark_1_e,
-                entity_mark_2_s, entity_mark_2_e,
+                entity_mark_1_s,
+                entity_mark_2_s,
                 masks, marks_only=False, entity_and_marks=False):
         outputs = self.model(text, attention_mask=masks, output_hidden_states=True)
         predictions = outputs.last_hidden_state
@@ -25,11 +25,15 @@ class TRCModel(nn.Module):
             e1_mark_tensors = outputs[torch.arange(outputs.size(0)), entity_mark_1_s]
             e2_mark_tensors = outputs[torch.arange(outputs.size(0)), entity_mark_2_s]
 
+            if entity_and_marks:
+                entity_1_tensor = outputs[torch.arange(outputs.size(0)), entity_1]
+                entity_2_tensor = outputs[torch.arange(outputs.size(0)), entity_2]
+
+                e1_mark_tensors = torch.cat((e1_mark_tensors, entity_1_tensor), 1)
+                e2_mark_tensors = torch.cat((e2_mark_tensors, entity_2_tensor), 1)
+
             entities_tensors = torch.cat((e1_mark_tensors, e2_mark_tensors), 1)
 
-            if entity_and_marks:
-                entity_1_tensor =
-
-            predictions = self.linear(entities_tensors)
+            dropout_output = self.dropout(entities_tensors)
+            predictions = self.linear(dropout_output)
         return predictions
-
